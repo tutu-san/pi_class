a2bd022fd53899de39e23bf1dc13455bf8f9245c
tutu-san <tumuri4071.dev@gmail.com>
2023-07-01 11:37:33 +0900

    fix

--- pi_ctrl.cpp
+++ pi_ctrl.cpp
@@ -8,15 +8,15 @@ float pid_class::pi_calc_rad(float current_rad){
     float calc_p = 0;
     
     //計算
-    calc_p = pid_class::current_target_rad - current_rad;
-    pid_class::integral += (calc_p + pid_class::before_p) / 2.0f * pid_class::DELTA_T;
+    calc_p = current_target_rad - current_rad;
+    integral += (calc_p + before_p) / 2.0f * DELTA_T;
 
     //ゲインとかけてみる
-    result_p = pid_class::gain_p * calc_p;
-    result_i = pid_class::gain_i * pid_class::integral;
+    result_p = gain_p * calc_p;
+    result_i = gain_i * integral;
 
     //次回計算用に今回のエラー値を保存
-    pid_class::before_p = calc_p;
+    before_p = calc_p;
 
     return result_p + result_i;
 }
@@ -25,37 +25,14 @@ float pid_class::pi_calc_rad(float current_rad){
 float pid_class::re_convert_rad(float current_data){
     float rad_per_sec = 0;
 
-    rad_per_sec = ((current_data * pid_class::pi) / 1024.0f) / pid_class::DELTA_T;
+    rad_per_sec = ((current_data * pi) / 1024.0f) / DELTA_T;
     
     return rad_per_sec;
 }
 
-//ゲインを変えたいときに
-void pid_class::gain_setup(float new_p_gain, float new_i_gain, float new_d_gain){
-    pid_class::gain_p = new_p_gain;
-    pid_class::gain_i = new_i_gain;
-    pid_class::gain_d = new_d_gain;
-
-    return;
-}
-
-//delta_t(ms)を変えたいときに
-void pid_class::delta_t_setup(float new_delta_t){
-    pid_class::DELTA_T = new_delta_t;
-
-    return;
-}
-
-//目標速度設定を変えたいときに
-void pid_class::terget_setup(float new_terget_rad){
-    pid_class::max_terget_rad = new_terget_rad;
-
-    return;
-}
-
 //現在の目標速度設定(コントローラーの入力をここで目標速度に変換する)
 void pid_class::update_target_spd(float spd_rate){
-    pid_class::current_target_rad = pid_class::max_terget_rad * spd_rate; //spd_rateは、コントローラーから得た入力を計算して、最高速度との割合で出す。
+    current_target_rad = max_terget_rad * (spd_rate * 0.01f); //spd_rateは、コントローラーから得た入力を計算して、最高速度との割合で出す。
 }
 
 //エンコーダーの入力から、PWMの値を求める(この前に、delayを入れておくこととする)
@@ -65,12 +42,12 @@ float pid_class::motor_calc(float current_data){
     rad_per_sec = pid_class::re_convert_rad(current_data);
 
     result_motor_pwm = pid_class::pi_calc_rad(rad_per_sec);
-
+/*
     if(result_motor_pwm < 0){
         result_motor_pwm = 0;
     }else if(result_motor_pwm > 65535){
         result_motor_pwm = 65535;
     }
-
+*/
     return result_motor_pwm;
 }
--- pi_ctrl.hpp
+++ pi_ctrl.hpp
@@ -9,18 +9,13 @@ class pid_class{
 private:
     //定数(piなど)
     const float pi = 3.1415926f;
-    const float default_gain_p = 2000.0f;
-    const float default_gain_i = 5.0f;
-    const float default_gain_d = 0.0f;
-    const float default_delta_t = 0.01f;
-    const float default_target_rad = 0.0f;
 
-    //計算時使用の定数(gain_setup関数で変更可能とする)
-    float gain_p = default_gain_p;
-    float gain_i = default_gain_i;
-    float gain_d = default_gain_d; //未実装のD制御
-    float DELTA_T = default_delta_t;
-    float max_terget_rad = default_target_rad;
+    //計算時使用の定数
+    float gain_p = 2000.0f;
+    float gain_i = 5.0f;
+    float gain_d = 0.0f; //未実装のD制御
+    float DELTA_T = 0.01f;
+    float max_terget_rad = 6.0f;
 
     float current_target_rad = 0.0f;
     float before_p = 0.0f; //前回のエラー値
@@ -32,9 +27,6 @@ private:
 public:
     float pi_calc_rad(float);
     float re_convert_rad(float);
-    void gain_setup(float, float, float);
-    void delta_t_setup(float);
-    void terget_setup(float);
     void update_target_spd(float);
     float motor_calc(float);
 };
